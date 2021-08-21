df6b1277816f6ebdcbc6c7531de54d7e2b703b42
Brenton Turnor <brenton.turnor@hotmail.com>
2021-08-21 12:43:51 +0800

    adding working server.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,10 +9,10 @@ INSERT INTO role (title, salary, department_id)
 VALUES
 ('Full Stack Developer', 80000, 1),
 ('Software Engineer', 120000, 1),
-('Accountant', 10000, 2), 
+('Accountant', 100000, 2), 
 ('Finanical Analyst', 150000, 2),
-('Sales Manager', 70000, 3), 
-('Sales Person', 90000, 3),
+('Sales Manager', 90000, 3), 
+('Sales Person', 70000, 3),
 ('Project Manager', 100000, 4),
 ('Operations Manager', 90000, 4);
 
